155974decedadb81e7ff3662c616a98d8c01b8cc
Elsdon Chang <chan2242@umn.edu>
2024-06-05 17:15:39 -0500

    moved /api/student endpoint to StudentController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,24 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/api/students")
-	public List<Student> getStudents() {
-		return List.of(new Student(1L, "Elsdon", "elsdonc21@gmail.com", 19, LocalDate.of(2004, 11, 21)));
-	}
 }
