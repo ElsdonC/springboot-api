5652655282b732e317cab03b5cd327487884d749
Elsdon Chang <chan2242@umn.edu>
2024-06-06 17:54:18 -0500

    created student service

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,13 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(new Student(1L, "Elsdon", "elsdonc21@gmail.com", 19, LocalDate.of(2004, 11, 21)));
+    }
+}
