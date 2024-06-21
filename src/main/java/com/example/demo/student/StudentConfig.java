1129523101bcb002b7d2c7f41addb2cdea3e0332
Elsdon Chang <chan2242@umn.edu>
2024-06-20 21:22:29 -0500

    removed age field from student instantiation

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -14,8 +14,8 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
-            Student student1 = new Student("student1", "student1@gmail.com", 19, LocalDate.of(2000, Month.NOVEMBER, 2));
-            Student student2 = new Student("student2", "student2@gmail.com", 18, LocalDate.of(2002, Month.NOVEMBER, 5));
+            Student student1 = new Student("student1", "student1@gmail.com", LocalDate.of(2000, Month.NOVEMBER, 2));
+            Student student2 = new Student("student2", "student2@gmail.com", LocalDate.of(2002, Month.NOVEMBER, 5));
             repository.saveAll(List.of(student1, student2));
         };
     }
